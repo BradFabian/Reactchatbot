49d15e026c64087592508309a20ce229ab920066
Brad <bradleofabian@gmail.com>
2020-07-26 17:19:24 -0400

    set up learning options

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import Chatbot from "react-chatbot-kit";
 import "./App.css";
 
+import ActionProvider from "./chatbot/ActionProvider";
+import MessageParser from "./chatbot/MessagerParser";
+import config from "./config/config";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+        />
       </header>
     </div>
   );
--- chatbot/src/chatbot/ActionProvider.js
+++ chatbot/src/chatbot/ActionProvider.js
@@ -1,8 +1,21 @@
-// ActionProvider starter code
 class ActionProvider {
   constructor(createChatBotMessage, setStateFunc) {
     this.createChatBotMessage = createChatBotMessage;
     this.setState = setStateFunc;
+  }
+
+  greet() {
+    const greetingMessage = this.createChatBotMessage("Hi, friend.");
+    this.updateChatbotState(greetingMessage);
+  }
+
+  updateChatbotState(message) {
+    // NOTE: This function is set in the constructor, and is passed in      // from the top level Chatbot component. The setState function here     // actually manipulates the top level state of the Chatbot, so it's     // important that we make sure that we preserve the previous state.
+
+    this.setState((prevState) => ({
+      ...prevState,
+      messages: [...prevState.messages, message],
+    }));
   }
 }
 
